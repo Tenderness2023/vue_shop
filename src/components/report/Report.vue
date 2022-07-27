5f63ce2204e7147c2c7742210b3c66be38160923
Tenderness2023 <3285544304@qq.com>
2022-07-27 16:28:48 +0800

    完成报表功能开发

---
+++ src/components/report/Report.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片试图区域 -->
+    <el-card>
+      <!--2. 为echarts准备一个具备大小的容器 -->
+      <div id="main" style="width: 800px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// 1. 导入echarts
+import * as echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  data() {
+    return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    }
+  },
+  async mounted() {
+    // 3. 基于准备好的dom,初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折线图数据失败')
+    }
+    // 4. 准备数据和配置项
+    const result = _.merge(res.data, this.options)
+    // 5. 绘制图表
+    myChart.setOption(result)
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
